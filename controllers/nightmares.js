6fc0c6b1b20f92b31cda80a92dbe664b25749bcb
Shak94 <shakoi.1994@gmail.com>
2023-05-24 20:08:21 -0400

    nightmare get route works

--- controllers/nightmares.js
+++ controllers/nightmares.js
@@ -23,6 +23,40 @@ router.get('/nightmares/:id', async (req, res) => {
     }
 
 });
+router.post('/nightmares', async (req, res, next) =>{
+    try{
+        const newNightmare= req.body
+        await Nightmare.create(req.body);
+        console.log(newNightmare);
+        res.redirect ('/nightmares')
+    } catch (error){
+        console.log(error)
+        next();
+    }
+});
+
+router.put('/dreams/:id', async (req, res, next) => {
+    try{
+        const editedDreams = await Dreams.findByIdAndUpdate(req.params.id, req.body);
+        res.redirect(`/dreams/${req.params.id}`);
+     } catch(error){
+        console.log(error);
+        next();
+     }
+
+});
+
+router.delete('/dreams/:id', async (req, res, next) => {
+    try{
+        const deletedDream = await Dreams.findByIdAndDelete(req.params.id);
+        res.redirect('/dreams')
+    
+    } catch(error){
+        console.log(error);
+        next();
+    }
+})
+module.exports = router;
 
 
 module.exports = router;
